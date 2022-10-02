b74f709a89c8373201ced5fdf25b9100f21d4d21
Takyo <takyomismo@hotmail.com>
2022-10-02 09:53:05 +0200

    Initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+const config = {
+  content: ["./index.html", './src/**/*.{svelte,js,ts}'], // for unused CSS
+  plugins: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+}
+
+module.exports = config;
