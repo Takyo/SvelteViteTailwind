a8a12fabd5d8730b26bb5c7ea7041bd24aa25f90
Takyo <takyomismo@hotmail.com>
2022-10-12 15:54:45 +0200

    Pequeños ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-// import postcss from './postcss.config.cjs';
-// import { svelte } from '@sveltejs/vite-plugin-svelte'
-// import { defineConfig } from 'vite'
-
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  // server: { port: 3000 },
 })
